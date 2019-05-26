4ecfd44fc43c15d3c87321a33dca598b7a610fec
Benjamin <got12g@gmail.com>
2019-05-26 11:17:18 -0400

    agrega componentes iniciales + css + landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { InicioComponent as GestionComponent } from './gestion/inicio/inicio.component';
+import { InicioComponent as CompraComponent } from './compra/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LandingComponent },
+  { path: "gestion", component: GestionComponent },
+  { path: "compra", component: CompraComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { InicioComponent as CompraComponent } from './compra/inicio/inicio.component';
+import { InicioComponent as GestionComponent } from './gestion/inicio/inicio.component';
+import { LoginComponent } from './gestion/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    CompraComponent,
+    GestionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
