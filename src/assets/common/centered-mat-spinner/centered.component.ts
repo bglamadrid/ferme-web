6157639bb446d30af2df6633bac4e7b37aae370d
Benjamin <got12g@gmail.com>
2019-06-14 14:22:30 -0400

    [limpieza] simplifica un componente muy común

--- src/assets/common/centered-mat-spinner/centered.component.ts
+++ src/assets/common/centered-mat-spinner/centered.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-centered-mat-spinner',
   template: `<div><mat-spinner></mat-spinner></div>`,
-  styleUrls: ['./centered.css']
+  styles: [
+    `div {
+      width: 100%;
+      height: 100%;
+      display: grid;
+      grid-template-columns: 1fr;
+      align-items: center;
+      justify-items: center;
+    }`
+  ]
 })
-export class CenteredMatSpinnerComponent implements OnInit {
+export class CenteredMatSpinnerComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
