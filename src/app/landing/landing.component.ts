4dd0729ad9355417c7656c3be5411f3487c6ef09
Benjamin La Madrid <got12g@gmail.com>
2019-07-13 16:38:23 -0400

    añade más contenido y estilos a landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./single-card-page.css']
+  styleUrls: [
+    './single-card-page.css',
+    './landing.component.css'
+  ]
 })
 export class LandingComponent implements OnInit {
 
