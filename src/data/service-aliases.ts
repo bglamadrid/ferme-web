b7d58a00a51f8a5b98cbfc89ba12e94016b030b8
Benjamin La Madrid <got12g@gmail.com>
2020-05-27 13:00:28 -0400

    added data service aliases

---
+++ src/data/service-aliases.ts
@@ -0,0 +1,11 @@
+export const SERVICE_ALIASES = {
+  auth: 'AuthData',
+  shared: 'SharedData',
+  clients: 'ClientsData',
+  employees: 'EmployeesData',
+  purchaseOrder: 'PurchaseOrderData',
+  products: 'ProductsData',
+  providers: 'ProvidersData',
+  users: 'UsersData',
+  sales: 'SalesData'
+};
