eedc2a802abdeee88b961a97c8354d393e71cabb
Benjamin La Madrid <got12g@gmail.com>
2020-06-10 13:10:50 -0400

    adds missing functionality to two data services

--- src/data/local-memory/ordenes_compra.local-memory-data.service.ts
+++ src/data/local-memory/ordenes_compra.local-memory-data.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { EntityLocalMemoryDataService } from './local-memory-data.abstract-service';
 import { OrdenCompra } from 'src/models/entities/OrdenCompra';
+import { CompositeEntityDataService } from '../composite-entity.data.iservice';
+import { DetalleOrdenCompra } from 'src/models/entities/DetalleOrdenCompra';
+import { Observable, of } from 'rxjs';
 
 export const MOCK_PURCHASE_ORDERS: Partial<OrdenCompra>[] = [
   {
@@ -11,11 +14,16 @@ export const MOCK_PURCHASE_ORDERS: Partial<OrdenCompra>[] = [
 
 @Injectable()
 export class OrdenCompraLocalMemoryDataService
-  extends EntityLocalMemoryDataService<OrdenCompra> {
+  extends EntityLocalMemoryDataService<OrdenCompra>
+  implements CompositeEntityDataService<OrdenCompra, DetalleOrdenCompra> {
 
   protected items: OrdenCompra[] = MOCK_PURCHASE_ORDERS.map(n => Object.assign(new OrdenCompra(), n));;
 
   constructor() {
     super();
+  }
+
+  readDetailsById(id: number): Observable<DetalleOrdenCompra[]> {
+    return of([]);
   }
 }
--- src/data/local-memory/ventas.local-memory-data.service.ts
+++ src/data/local-memory/ventas.local-memory-data.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { EntityLocalMemoryDataService } from './local-memory-data.abstract-service';
-import { OrdenCompra } from 'src/models/entities/OrdenCompra';
 import { Venta } from 'src/models/entities/Venta';
+import { DetalleVenta } from 'src/models/entities/DetalleVenta';
+import { CompositeEntityDataService } from '../composite-entity.data.iservice';
+import { Observable, of } from 'rxjs';
 
 export const MOCK_SALES: Partial<Venta>[] = [
   {
@@ -12,11 +14,16 @@ export const MOCK_SALES: Partial<Venta>[] = [
 
 @Injectable()
 export class VentasLocalMemoryDataService
-  extends EntityLocalMemoryDataService<Venta> {
+  extends EntityLocalMemoryDataService<Venta>
+  implements CompositeEntityDataService<Venta, DetalleVenta> {
 
   protected items: Venta[] = MOCK_SALES.map(n => Object.assign(new Venta(), n));
 
   constructor() {
     super();
+  }
+
+  readDetailsById(id: number): Observable<DetalleVenta[]> {
+    return of([]);
   }
 }
