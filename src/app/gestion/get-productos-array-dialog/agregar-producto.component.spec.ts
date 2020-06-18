c67f3ef667de05b098f69c75ecfe48cabee436e1
Benjamin La Madrid <got12g@gmail.com>
2020-06-18 12:33:31 -0400

    fixes component class name in test

--- src/app/gestion/get-productos-array-dialog/agregar-producto.component.spec.ts
+++ src/app/gestion/get-productos-array-dialog/agregar-producto.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AgregarProductoDialogComponent } from './agregar-producto.component';
 
-describe('AgregarProductoComponent', () => {
+describe('AgregarProductoDialogComponent', () => {
   let component: AgregarProductoDialogComponent;
   let fixture: ComponentFixture<AgregarProductoDialogComponent>;
 
